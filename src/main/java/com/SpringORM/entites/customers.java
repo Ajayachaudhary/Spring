42482c88e1aef29768a532a5c70fe4d041ef2adf
Ajaya Chaudhary <ajaychy0602@gmail.com>
2024-07-23 14:27:18 +0545

    Add com.SpringORM.entites package and Customers.java file

---
+++ src/main/java/com/SpringORM/entites/customers.java
@@ -0,0 +1,36 @@
+package com.SpringORM.entites;
+
+public class customers {
+
+	private int customerID;
+	
+	private String customerName;
+	
+	private String customerAddress;
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+	
+	
+}
